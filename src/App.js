6de7cc6b0b5a6de6e986b8b80bd0e33555992d95
hamdtc <mohamedtc73@gmail.com>
2023-05-20 19:47:02 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import TableRow from './Components/TableRow';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import TableRow from "./TableRow";
 
 const App = () => {
-
   let [data, setData] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
       .then(function (response) {
         console.log(response);
         setData(response.data);
@@ -20,15 +21,13 @@ const App = () => {
         console.log(error);
       });
   }, []);
-  
-
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Top 10 Cryptocurrencies</h1>
       <br></br>
-       <table className='table table-hover table-bordered'>
-        <thead className='table-dark'>
+      <table className="table table-hover table-bordered">
+        <thead className="table-dark">
           <tr>
             <th>Name</th>
             <th>ID</th>
@@ -39,14 +38,12 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-        {data.length > 0 && data.map((coin) => (
-            <TableRow key={coin.id} coin={coin} />
-        ))}
+          {data.length > 0 &&
+            data.map((coin) => <TableRow key={coin.id} coin={coin} />)}
         </tbody>
       </table>
     </div>
-  )
-
-}
+  );
+};
 
 export default App;
